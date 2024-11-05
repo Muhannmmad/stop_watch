74b765889ea3ea0c49155bf28db08716c60dbada
Muhammad <muhannmmad@gmail.com>
2024-11-05 16:05:47 +0100

    start made

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,40 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(stopwatch());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class stopwatch extends StatelessWidget {
+  const stopwatch({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      title: 'Stopwatch',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: stopwatchPage(),
+    );
+  }
+}
+
+class stopwatchPage extends StatefulWidget {
+  const stopwatchPage({super.key});
+
+  @override
+  _stopwatchPageState createState() => _stopwatchPageState();
+}
+
+class _stopwatchPageState extends State<stopwatchPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Stopwatch')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Start functionality will be added here
+          },
+          child: Text('Start'),
         ),
       ),
     );
